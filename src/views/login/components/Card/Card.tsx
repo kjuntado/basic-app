4159202c08ae65d6465bcc6a3a489e35c87a7b49
Katreena Gabrielle Juntado <katreenajuntado@gamil.com>
2022-06-08 22:39:23 +0800

    Add state handling + login button

--- src/views/login/components/Card/Card.tsx
+++ src/views/login/components/Card/Card.tsx
@@ -1,52 +1,84 @@
-import React, { MouseEvent } from "react";
+import React, { ChangeEvent, MouseEvent, useState } from "react";
 import {
+    Button,
     FormControl,
     IconButton,
     InputAdornment,
     InputLabel,
-    OutlinedInput,
-    TextField
+    OutlinedInput
 } from "@mui/material";
-import { VisibilityOff } from "@mui/icons-material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 import { Card, Title } from "./Card.style";
 
 export const CardComponent = () => {
+    // Local States
+    const [branchId, setBranchId] = useState<string>('');
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
+    // Event handlers
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
+
     const handleMouseDownPassword = (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
     };
 
+    const handleChange = (type: string) => (event: ChangeEvent<HTMLInputElement>) => {
+        switch (type) {
+            case 'branchId':
+                setBranchId(event.target.value);
+                break;
+            case 'username':
+                setUsername(event.target.value);
+                break;
+            case 'password':
+                setPassword(event.target.value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    // Render functions
     const renderTextField = (label: string, id: string) => {
         return (
-            <TextField
-                label={label}
-                id={`${id}-input`}
-                margin="normal"
-            />
+            <FormControl sx={{ mt: 2, mb: 1 }} variant="outlined">
+                <InputLabel htmlFor={`${id}-input`}>
+                    {label}
+                </InputLabel>
+                <OutlinedInput
+                    id={`${id}-input`}
+                    type="text"
+                    value={id === 'branchId' ? branchId : username}
+                    onChange={handleChange(id)}
+                    label={label}
+                />
+            </FormControl>
         );
     };
 
     const renderPassword = () => {
         return (
-            <FormControl sx={{ mt: 2, mb: 1 }} variant="outlined">
+            <FormControl sx={{ mt: 2, mb: 3 }} variant="outlined">
                 <InputLabel htmlFor="pw-input">
                     Password
                 </InputLabel>
                 <OutlinedInput
                     id="pw-input"
-                    //type={values.showPassword ? 'text' : 'password'}
-                    type="password"
-                    //value={values.password}
-                    //onChange={handleChange('password')}
+                    type={showPassword ? 'text' : 'password'}
+                    value={password}
+                    onChange={handleChange('password')}
                     endAdornment={
                     <InputAdornment position="end">
                         <IconButton
                             aria-label="toggle password visibility"
-                            //onClick={handleClickShowPassword}
+                            onClick={handleClickShowPassword}
                             onMouseDown={handleMouseDownPassword}
                             edge="end"
-                            >
-                            <VisibilityOff />
+                        >
+                            {showPassword ? <Visibility /> : <VisibilityOff />}
                         </IconButton>
                     </InputAdornment>
                     }
@@ -59,9 +91,10 @@ export const CardComponent = () => {
     return (
         <Card>
             <Title>Login</Title>
-            {renderTextField('Branch ID', 'branch-id')}
+            {renderTextField('Branch ID', 'branchId')}
             {renderTextField('Username', 'username')}
             {renderPassword()}
+            <Button variant="contained" size="large">LOGIN</Button>
         </Card>
     );
 };
